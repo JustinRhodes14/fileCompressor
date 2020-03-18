19f8fc9cbe38fb24e5a47a061216b5530f504899
Justin <rhodesju21@gmail.com>
2020-03-18 17:22:20 -0400

    added BST functionality and stuff

--- fileCompressor.c
+++ fileCompressor.c
@@ -1,5 +1,6 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 //BSTNode structure, to create one follow following format:
 //	Node* <name> = (Node*)malloc(sizeof(Node));
 //Making an array of BSTNodes is as follows:
@@ -26,24 +27,39 @@ typedef struct _heapItem { //need to see whether or not tree is set
 
 heapItem* heapInit();//initializes the heap with frequencies of -1
 void printHeap();//prints out all the frequencies within the heap and their words
+void heapify();//converts our array into a heap after we read in all the data
+char* copyString(char*,char*);//copies a string from another word and properly inserts null terminator
+void bstInsert(char*);//inserts a word into our bst in alphabetical ordering
+int bstSearch(char*);//searches for bst item and increments it's frequency if found
+int compareString(char*,char*);//compares two strings and returns a negative number if the first one is lesser, and a pos number if the first one is greater 
+void printBst(Node*);//prints out the BST items
 
-Node* root;//our tree node
+Node* root;//our tree node, initially we store all the values in here and then into our heap
+int nodeCount = 0;//amount of items in our tree
+
+boolean rootSet = false;//indicates whether or not root is set
+
 heapItem* heapArr;//our array of vals to start our huffman tree
-int heapSize = 10;//starting heap size, will update and rehash as we continue to read in words, then heapify at the end into a min heap
+
+int heapSize = 10; //temporary, for testing purposes
 
 int main(int argc, char** argv) {
 	
+	bstInsert("dog\0");
+	bstInsert("cat\0");
+	bstInsert("dog\0");
+	bstInsert("bat\0");
 	heapArr = heapInit();
 	printHeap();
-	
+	printBst(root);
 	return 0;
 }
 
 heapItem* heapInit() {
 
-	heapItem* arr = (heapItem*)malloc(heapSize * sizeof(heapItem));
+	heapItem* arr = (heapItem*)malloc(nodeCount * sizeof(heapItem));
 	int i;
-	for (i = 0; i < heapSize; i++) {
+	for (i = 0; i < nodeCount; i++) {
 		arr[i].freq = -1; //indicates no item is inserted
 		arr[i].isTree = false;
 	}
@@ -52,14 +68,123 @@ heapItem* heapInit() {
 }
 
 void printHeap() {
+
 	int i;
-	for (i = 0; i < heapSize; i++) {
+	for (i = 0; i < nodeCount; i++) {
 		printf("%s: %d, %d\n",heapArr[i].word,heapArr[i].freq,heapArr[i].isTree);
+	}	
+
+}
+
+void bstInsert(char* word) {
+	if (rootSet == false) {
+		root = (Node*)malloc(sizeof(Node));
+		root->freq = 1;
+		root->word = copyString(root->word,word);
+		root->left = NULL;
+		root->right = NULL;
+		root->freqSet = true;
+		rootSet = true;
+		nodeCount++;
+	} else if (bstSearch(word) == 1) {//1 indicates successfully found and incremented frequency
+		return;
+	} else {
+		Node* prev;
+		Node* ptr = root;
+		boolean left = false;
+		while (ptr != NULL) {
+			if (compareString(ptr->word, word) > 0) {
+				prev = ptr;
+				ptr = ptr->right;
+				left = false;
+			} else if (compareString(ptr->word,word) < 0) {
+				prev = ptr;
+				ptr = ptr->left;
+				left = true;
+			}
+		}
+		Node* temp = (Node*)malloc(sizeof(Node));
+		temp->freq = 1;
+		temp->word = copyString(temp->word,word);
+		temp->left = NULL;
+		temp->right = NULL;
+		temp->freqSet = true;
+		if (left == true) {
+			prev->left = temp;
+		} else {
+			prev->right = temp;
+		}	
+		nodeCount++;
 	}	
 }
 
+int bstSearch(char* word) {
+	Node* ptr = root;
+	while (ptr != NULL) {
+		if (compareString(ptr->word,word) < 0) {
+			ptr = ptr->right;
+		} else if (compareString(ptr->word,word) > 0) {
+			ptr = ptr->left;
+		} else {
+			ptr->freq+= 1;
+			return 1;
+		}
+	}
+	return -1;//not found
+}
 
+void printBst(Node* ptr) {
+	if (ptr == NULL) {
+		return;
+	}
+	
+	printBst(ptr->left);
+	printf("BST_NODE: %s: %d\n",ptr->word,ptr->freq);
+	printBst(ptr->right);
+}
 
+char* copyString(char* to, char* from) {
+	int length = strlen(from);
+	to = (char*)malloc(length * sizeof(char) + 1);
+	int i;
+	for ( i = 0; i < length; i++) {
+		to[i] = from[i];
+	}
+	to[length] = '\0';
+	return to;
+}
+
+int compareString(char* str1, char* str2) {
+	int len1 = strlen(str1);
+	int len2 = strlen(str2);
+	int shorter = 0;
+	int len = len1;
+	if (len1 > len2) {
+		shorter = 1;//first one is shorter
+		len = len1;
+	} else if (len2 < len1) {
+		shorter = 2;//second one is shorter
+		len = len2;
+	} else {
+		shorter = 0;//equal length
+		len = len1;
+	}
+	
+	int i;
+	for ( i = 0; i < len; i++) {
+		if (str1[i] != str2[i]) {
+			return ((int)str1[i] - (int)str2[i]);//negative if str1 is lesser, positive if str1 is greater
+		}	
+	}
+	if (len1 < len2) {
+		return -1;
+	} else if (len2 < len1) {
+		return 1;
+	}
+	return 0;//equal strings
+	
+	
+}
 
 
 
